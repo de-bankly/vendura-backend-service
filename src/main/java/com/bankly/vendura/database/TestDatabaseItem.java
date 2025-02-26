a67517d41dfd339fc063bc29a03fd5b6fc30cc56
Anton Bowe <mail@antonbowe.de>
2025-02-26 20:06:41 +0100

    VND-7 establish database connection and create sample objects

---
+++ src/main/java/com/bankly/vendura/database/TestDatabaseItem.java
@@ -0,0 +1,23 @@
+package com.bankly.vendura.database;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * This object is for the purpose of demonstration
+ */
+@Data // convenient shortcut annotation for ToString, EqualsAndHashCode, Getter, Setter, RequiredArgsConstructor
+@Document("TestDatabaseItem") // Specifies the MongoDB collection
+@NoArgsConstructor // Required for all database "Documents"
+@AllArgsConstructor // Generates a constructor for all fields
+public class TestDatabaseItem {
+
+    @Id // Primary key of the database
+    private String id;
+
+    private String name; // database property
+
+}
