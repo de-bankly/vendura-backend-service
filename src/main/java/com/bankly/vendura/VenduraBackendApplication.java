378ce24858aa6ad309a435659e66b2c8fad307b6
Anton Bowe <mail@antonbowe.de>
2025-02-26 19:03:24 +0100

    VND-2 initial commit

---
+++ src/main/java/com/bankly/vendura/VenduraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bankly.vendura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenduraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenduraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankly/vendura/VenduraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankly.vendura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenduraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
