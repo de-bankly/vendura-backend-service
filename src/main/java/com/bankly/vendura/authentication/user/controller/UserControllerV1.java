6527e980c64988b9105d4ab2e3397350021a6625
Anton Bowe <mail@antonbowe.de>
2025-03-11 19:26:14 +0100

    Updates to UserControllerV1

--- src/main/java/com/bankly/vendura/authentication/user/controller/UserControllerV1.java
+++ src/main/java/com/bankly/vendura/authentication/user/controller/UserControllerV1.java
@@ -1,20 +1,19 @@
 package com.bankly.vendura.authentication.user.controller;
 
-import com.bankly.vendura.authentication.roles.model.RoleDTO;
 import com.bankly.vendura.authentication.user.UserService;
 import com.bankly.vendura.authentication.user.model.User;
 import com.bankly.vendura.authentication.user.model.UserDTO;
-import java.util.stream.Collectors;
+import com.bankly.vendura.authentication.user.model.UserRepository;
+import com.bankly.vendura.utilities.exceptions.EntityRetrieveException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/user")
@@ -22,25 +21,38 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserControllerV1 {
 
   private final UserService userService;
+  private final UserRepository userRepository;
 
   @PreAuthorize("hasRole('ADMIN')")
   @RequestMapping(value = "/create", method = RequestMethod.POST)
   public ResponseEntity<?> createUser(@RequestBody UserDTO userDTO) {
-    User user =
-        this.userService.createUser(
-            userDTO.getUsername(),
-            userDTO.getPassword(),
-            userDTO.getRoles().stream().map(RoleDTO::getName).collect(Collectors.toSet()));
-
+    User user = this.userService.createUser(userDTO);
     return ResponseEntity.ok().body(UserDTO.fromUser(user));
   }
 
-  @RequestMapping(value = "/own", method = RequestMethod.GET)
-  public ResponseEntity<?> getOwnUser() {
-    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    String username = authentication.getName();
-
-    User user = this.userService.getUser(username);
+  @GetMapping("/me")
+  public ResponseEntity<?> getUser(Authentication authentication) {
+    User user = this.userService.getUser(authentication.getName());
     return ResponseEntity.ok().body(UserDTO.fromUser(user));
+  }
+
+  @GetMapping
+  @PreAuthorize("hasRole('ADMIN')")
+  public ResponseEntity<?> getAllUsers(Pageable pageable) {
+    Page<User> users = this.userRepository.findAll(pageable);
+    return ResponseEntity.ok().body(users.map(UserDTO::fromUser));
+  }
+
+  @GetMapping("/{id}")
+  @PreAuthorize("hasRole('ADMIN')")
+  public ResponseEntity<?> getUserById(@PathVariable("id") String id) {
+    return ResponseEntity.ok(
+        UserDTO.fromUser(
+            this.userRepository
+                .findById(id)
+                .orElseThrow(
+                    () ->
+                        new EntityRetrieveException(
+                            "User with id " + id + " not found", HttpStatus.NOT_FOUND, id))));
   }
 }
