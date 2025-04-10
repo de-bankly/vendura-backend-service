1de7414aef42ed5f469b31f2dc5234a4ab4de343
Anton Bowe <mail@antonbowe.de>
2025-04-10 13:39:18 +0200

    Introduced first approach of SALE

---
+++ src/main/java/com/bankly/vendura/sale/model/Sale.java
@@ -0,0 +1,53 @@
+package com.bankly.vendura.sale.model;
+
+import com.bankly.vendura.authentication.user.model.User;
+import com.bankly.vendura.inventory.product.model.Product;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "sales")
+public class Sale {
+
+  @Id private String id;
+
+  private Date date;
+  @DBRef private User cashier;
+  private Set<Position> positions;
+
+  @Getter
+  @Builder
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class Position {
+    @DBRef private Product product;
+    private int quantity;
+    private Set<Position> connectedPositions = new HashSet<>();
+
+    public void setQuantity(int quantity) {
+      this.quantity = quantity;
+      for (Position connectedPosition : this.connectedPositions) {
+        connectedPosition.setQuantity(quantity);
+      }
+    }
+
+    public void setProduct(Product product) {
+      this.product = product;
+      this.connectedPositions =
+          product.getConnectedProducts().stream()
+              .map(
+                  connectedProduct ->
+                      Position.builder().product(connectedProduct).quantity(this.quantity).build())
+              .collect(Collectors.toSet());
+    }
+  }
+}
