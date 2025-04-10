00fb61c113e0525e03cf073ff68b09d039a99843
Anton Bowe <mail@antonbowe.de>
2025-04-10 13:45:03 +0200

    Preventing endpoints from creating Products, ProductCategories and Brands with the same name more than one time closes VND-120

--- src/main/java/com/bankly/vendura/inventory/brand/model/BrandRepository.java
+++ src/main/java/com/bankly/vendura/inventory/brand/model/BrandRepository.java
@@ -2,4 +2,10 @@ package com.bankly.vendura.inventory.brand.model;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface BrandRepository extends MongoRepository<Brand, String> {}
+import java.util.Optional;
+
+public interface BrandRepository extends MongoRepository<Brand, String> {
+
+    Optional<Brand> findByName(String name);
+
+}
--- src/main/java/com/bankly/vendura/inventory/productcategory/ProductCategoryService.java
+++ src/main/java/com/bankly/vendura/inventory/productcategory/ProductCategoryService.java
@@ -3,6 +3,7 @@ package com.bankly.vendura.inventory.productcategory;
 import com.bankly.vendura.inventory.productcategory.model.ProductCategory;
 import com.bankly.vendura.inventory.productcategory.model.ProductCategoryDTO;
 import com.bankly.vendura.inventory.productcategory.model.ProductCategoryRepository;
+import com.bankly.vendura.utilities.exceptions.EntityCreationException;
 import com.bankly.vendura.utilities.exceptions.EntityRetrieveException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,15 @@ public class ProductCategoryService {
   private final ProductCategoryRepository productCategoryRepository;
 
   public ProductCategory create(ProductCategoryDTO productCategoryDTO) {
+
+    if (this.productCategoryRepository.findByName(productCategoryDTO.getName()).isPresent()) {
+      throw new EntityCreationException(
+          "ProductCategory with that name already exists",
+          HttpStatus.CONFLICT,
+          productCategoryDTO.getName(),
+          false);
+    }
+
     ProductCategory productCategory = new ProductCategory();
     productCategory.setName(productCategoryDTO.getName());
     return this.productCategoryRepository.save(productCategory);
@@ -31,6 +41,15 @@ public class ProductCategoryService {
                         "Product category not found", HttpStatus.NOT_FOUND, id));
 
     if (productCategoryDTO.getName() != null) {
+
+      if (this.productCategoryRepository.findByName(productCategoryDTO.getName()).isPresent()) {
+        throw new EntityCreationException(
+            "ProductCategory with that name already exists",
+            HttpStatus.CONFLICT,
+            productCategoryDTO.getName(),
+            false);
+      }
+
       productCategory.setName(productCategoryDTO.getName());
     }
 
