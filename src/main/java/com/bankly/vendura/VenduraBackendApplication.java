38f7fd04455fb51cb8f1cbb8c4f1832a3f0aacbb
Anton Bowe <54149917+antonbowe@users.noreply.github.com>
2025-04-11 13:24:40 +0200

    Update VenduraBackendApplication.java

--- src/main/java/com/bankly/vendura/VenduraBackendApplication.java
+++ src/main/java/com/bankly/vendura/VenduraBackendApplication.java
@@ -8,6 +8,7 @@ public class VenduraBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VenduraBackendApplication.class, args);
+		System.out.println("XXX");
 	}
 
 }
