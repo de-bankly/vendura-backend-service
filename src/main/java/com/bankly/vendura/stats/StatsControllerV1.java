6e44ca41c9c54535b62d3d50022f7f2f2fccf737
Anton Bowe <mail@antonbowe.de>
2025-04-23 22:23:29 +0200

    Changes

--- src/main/java/com/bankly/vendura/stats/StatsControllerV1.java
+++ src/main/java/com/bankly/vendura/stats/StatsControllerV1.java
@@ -1,8 +1,11 @@
 package com.bankly.vendura.stats;
 
+import com.bankly.vendura.inventory.product.model.Product;
 import com.bankly.vendura.stats.model.SummaryDTO;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -32,5 +35,13 @@ public class StatsControllerV1 {
                 System.currentTimeMillis()
                     - (long) SummaryDTO.Period.fromString(period).getDays() * 24 * 60 * 60 * 1000),
             new Date()));
+  }
+
+  @GetMapping("/topSellingProducts")
+  public ResponseEntity<Map<Product, Integer>> getTopSellingProducts(
+      @RequestParam(value = "period", required = false, defaultValue = "MONTH") String period,
+      @RequestParam(value = "limit", required = false, defaultValue = "10") int limit) {
+    return ResponseEntity.ok(
+        this.statsService.getProductsTopSelling(SummaryDTO.Period.fromString(period), limit));
   }
 }
