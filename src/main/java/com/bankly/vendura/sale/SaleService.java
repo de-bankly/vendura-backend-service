595398de8b8d0e862d1354d5d3a67431c09ee49e
Anton Bowe <mail@antonbowe.de>
2025-04-23 22:23:29 +0200

    Changes

--- src/main/java/com/bankly/vendura/sale/SaleService.java
+++ src/main/java/com/bankly/vendura/sale/SaleService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
--- src/main/java/com/bankly/vendura/stats/StatsService.java
+++ src/main/java/com/bankly/vendura/stats/StatsService.java
@@ -1,6 +1,7 @@
 package com.bankly.vendura.stats;
 
 import com.bankly.vendura.inventory.product.model.Product;
+import com.bankly.vendura.inventory.product.model.ProductRepository;
 import com.bankly.vendura.payment.model.*;
 import com.bankly.vendura.sale.model.Sale;
 import com.bankly.vendura.sale.model.SaleRepository;
@@ -8,7 +9,14 @@ import com.bankly.vendura.stats.model.SummaryDTO;
 import java.util.*;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.aggregation.ConvertOperators;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +25,8 @@ public class StatsService {
 
   private final SaleRepository saleRepository;
   private final PaymentRepository paymentRepository;
+  private final MongoTemplate mongoTemplate;
+  private final ProductRepository productRepository;
 
   public SummaryDTO getSummary(SummaryDTO.Period period) {
     Date date = new Date(System.currentTimeMillis() - (period.getDays() * 24 * 60 * 60 * 1000L));
@@ -120,6 +130,47 @@ public class StatsService {
                 e -> e.getKey().getId(), Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
   }
 
+  public Map<Product, Integer> getProductsTopSelling(SummaryDTO.Period period, int limit) {
+    Date periodStartDate =
+        new Date(System.currentTimeMillis() - (period.getDays()) * 24 * 60 * 60 * 1000L);
+    Date periodEndDate = new Date();
 
+    System.out.println(periodStartDate + " " + periodEndDate + " " + limit);
 
+    Aggregation aggregation =
+        Aggregation.newAggregation(
+            Aggregation.match(
+                Criteria.where("date")
+                    .gte(periodStartDate)
+                    .lte(periodEndDate)), // Sales within the period
+            Aggregation.unwind("positions"), // Positions the sales
+            Aggregation.replaceRoot("positions"),
+            Aggregation.addFields()
+                .addFieldWithValue("productId", "$product.$id") // $product.$id
+                .build(), // Positions, productId
+            Aggregation.group("productId")
+                .sum("quantity")
+                .as("totalQuantity"), // productId, positions, totalQuantity
+            Aggregation.sort(Sort.by(Sort.Order.desc("totalQuantity"))),
+            Aggregation.limit(limit),
+            Aggregation.lookup("products", "$_id", "$id", "product") // productId, totalQuantity
+            /*Aggregation.addFields()
+                .addFieldWithValue(
+                    "productObjectId", ConvertOperators.ToObjectId.toObjectId("$_id"))
+                .build(),*/
+            );
+
+    AggregationResults<Document> result =
+        this.mongoTemplate.aggregate(aggregation, "sales", Document.class);
+
+    List<String> productIds = result.getMappedResults().stream().map(doc -> doc.getString("productId")).toList();
+    List<Product> products = this.productRepository.findAllById(productIds);
+
+    Map<Product, Integer> productMap = new HashMap<>();
+    for (int i = 0; i < productIds.size(); i++) {
+      productMap.put(products.get(i), result.getMappedResults().get(i).getInteger("totalQuantity"));
+    }
+
+    return productMap;
+  }
 }
